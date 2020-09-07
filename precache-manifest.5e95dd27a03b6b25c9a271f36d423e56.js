a9c9f947ae3b9ad6a01b9793a2fd1413f7662862
Robel Tessema <robeltessema.h@gmail.com>
2020-09-07 13:51:58 -0500

    Updates

---
+++ precache-manifest.5e95dd27a03b6b25c9a271f36d423e56.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e56d41997f2a7b7769b7aa4d4349f7e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "13cff6cb90a69cd7549e",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "b27b90a20bf8bab77b9c",
+    "url": "/static/js/2.eacadcdb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.eacadcdb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13cff6cb90a69cd7549e",
+    "url": "/static/js/main.24ec3dec.chunk.js"
+  },
+  {
+    "revision": "67ed8a3ac10318b4a740",
+    "url": "/static/js/runtime-main.1345e647.js"
+  },
+  {
+    "revision": "16950e7d83b543e419f9ee770730777b",
+    "url": "/static/media/profile.16950e7d.jpg"
+  }
+]);
